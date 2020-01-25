1154cba65c4e2fbb033fafbed5dafa7632bf1208
joan.rosellr <joan.rosellr@gmail.com>
2020-01-25 20:40:49 +0100

    fix path

--- controller/controller_userDetail.php
+++ controller/controller_userDetail.php
@@ -4,5 +4,5 @@ $client_info = getClientInfo($_SESSION['user_id']);
 $img_path = '/img/clients/default_user.png';
 
 if (!empty($client_info['ProfileImg']))
-    $img_path = $publicPath . $client_info['ProfileImg'];
+    $img_path = $publicPath . 'clients/' . $client_info['ProfileImg'];
 require_once __DIR__ . '/../view/view_userDetail.php';
