89f4cc86be015e77ebbe26cb25dd53ac5a7d0e85
joan.rosellr <joan.rosellr@gmail.com>
2020-01-25 19:53:08 +0100

    img path

--- controller/controller_userDetail.php
+++ controller/controller_userDetail.php
@@ -1,8 +1,8 @@
 <?php
 require_once __DIR__ . '/../model/model_getUserInfo.php';
 $client_info = getClientInfo($_SESSION['user_id']);
-$img_path = __DIR__ . '/../img/default_user.png';
+$img_path = '/img/default_user.png';
 
 if (!empty($client_info['ProfileImg']))
-    $img_path = __DIR__ . '/..' . $publicPath . $client_info['ProfileImg'];
+    $img_path = $publicPath . $client_info['ProfileImg'];
 require_once __DIR__ . '/../view/view_userDetail.php';
