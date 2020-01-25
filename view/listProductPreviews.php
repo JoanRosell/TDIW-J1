fb65350dd233b8f7fc3abd058ee476431e2b938d
joan.rosellr <joan.rosellr@gmail.com>
2020-01-25 23:03:49 +0100

    add max width to cards

--- view/listProductPreviews.php
+++ view/listProductPreviews.php
@@ -1,5 +1,5 @@
 <?php foreach($products as $product) : ?>
-    <article id="<?php echo $product['ProductID']; ?>" class="productRef grid-item">
+    <article id="<?php echo $product['ProductID']; ?>" class="productRef grid-item card">
         <img src="<?php echo $product['Image']; ?>" alt="product image">
         <h3><?php echo htmlentities($product['Name']); ?></h3>
         <p> Price: <?php echo htmlentities($product['Price']); ?>&euro;</p>
