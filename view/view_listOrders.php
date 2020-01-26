20c450697dea10f16efc3d9afba406648a820b1d
joan.rosellr <joan.rosellr@gmail.com>
2020-01-26 14:33:40 +0100

    fix layout

--- view/view_listOrders.php
+++ view/view_listOrders.php
@@ -1,12 +1,12 @@
 <?php
 if (!empty($orders)) {
     foreach ($orders['invoices'] as $invoice): ?>
-        <section class="grid-item">
-            <p>Date of purchase: <?php echo htmlentities($invoice['date'], ENT_HTML5 | ENT_QUOTES); ?></p>
-            <p>Total price charged: <?php echo htmlentities($invoice['total_price'], ENT_HTML5 | ENT_QUOTES); ?>&euro;</p>
-            <p>Number of units purchased: <?php echo htmlentities($invoice['qty'], ENT_HTML5 | ENT_QUOTES); ?></p>
-            <p>List of products purchased:</p>
-            <div class="verticalGrid">
+        <section class="grid-item verticalGrid">
+            <p class="grid-item">Date of purchase: <?php echo htmlentities($invoice['date'], ENT_HTML5 | ENT_QUOTES); ?></p>
+            <p class="grid-item">Total price charged: <?php echo htmlentities($invoice['total_price'], ENT_HTML5 | ENT_QUOTES); ?>&euro;</p>
+            <p class="grid-item">Number of units purchased: <?php echo htmlentities($invoice['qty'], ENT_HTML5 | ENT_QUOTES); ?></p>
+            <p class="grid-item">List of products purchased:</p>
+            <div class="grid-item verticalGrid">
                 <?php foreach ($invoice['sales'] as $sale): ?>
                     <article class="grid-item">
                         <img src="<?php echo $sale['info']['Image']; ?>" alt="Product Image">
