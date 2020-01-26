f28715699705c178e1b8304cb5d3cc6aed2d5715
joan.rosellr <joan.rosellr@gmail.com>
2020-01-26 17:48:44 +0100

    prepare css for deployment

--- resource_purchaseConfirmation.php
+++ resource_purchaseConfirmation.php
@@ -3,7 +3,7 @@
     <title>Confirmation</title>
     <meta name="author" content="Joan Rosell">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <link rel="stylesheet" href="css/main.css?v= <?php echo time(); ?>">
+    <link rel="stylesheet" href="css/main.css">
     <script
             src="https://code.jquery.com/jquery-3.4.1.min.js"
             integrity="sha256-CSXorXvZcTkaix6Yvo6HppcZGetbYMGWSFlBw8HfCJo="
